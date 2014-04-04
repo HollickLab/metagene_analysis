e7a9fc4766e1cdcac49faf785d579a916fa1271c
Joy-El <joyel.barbour@gmail.com>
2014-04-04 10:01:17 -0400

    Early start of metagene_plot.py

---
+++ metagene_plot.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+'''The third step of metagene_analysis, metagene_plot.py uses R to create the
+metagene plot (as a PDF) and its associated statistics.
+Please see README for full details and examples.
+
+Requires:
+    python 2 (https://www.python.org/downloads/)
+    R (http://cran.us.r-project.org/)
+
+Joy-El R.B. Talbot Copyright (c) 2014
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+import sys, subprocess, re, datetime
+import argparse		# to parse the command line arguments
+
+PROGRAM = "metagene_plot.py"
+VERSION = "0.1.0"
+UPDATED = "1404034 JRBT"
+
+def get_arguments():
+    '''Collect and parse information from the user's command line arguments.'''
+    
+    date = datetime.datetime.now().strftime('%y%m%d-%H%M%S')
+    
+    parser = argparse.ArgumentParser(description=
+    '''The third step of metagene_analysis, metagene_plot.py uses R to create the
+metagene plot (as a PDF) and its associated statistics.
+Please see README for full details and examples.
+
+Requires:
+    python 2 (https://www.python.org/downloads/)
+    R (http://cran.us.r-project.org/)
+    ''')
+
+    parser.add_argument("-v","--version",
+                        action = 'version',
+                        version = "{} {}\tUpdated {}".format(PROGRAM, VERSION, UPDATED))
+    parser.add_argument("-a","--fileset_a",
+                        help = "Bin files from metagene_bin.py",
+                        metavar = 'BIN_FILE',
+                        required = True,
+                        action = 'append')
+    parser.add_argument("-b","--fileset_b",
+                        help = "Bin file from metagene_bin.py",
+                        metavar = 'BIN_FILE',
+                        required = True,
+                        action = 'append')                   
+    parser.add_argument("-o", "--output_prefix",
+                        help = "Prefix for output files",
+                        required = True)
+    
+    parser.add_argument("--normalization_a",
+                        help = "Normalization value for file_a; eg total reads would result in reads per million",
+                        type = int,
+                        required = True)
+    parser.add_argument("--normalization_b",
+                        help = "Normalization value for file_b; eg total reads would result in reads per million",
+                        type = int,
+                        required = True) 
+                                          
+    parser.add_argument("--feature_counted",
+                        help = "Name of feature examined, eg TSS, gene, intron",
+                        required = True)
+
+    return parser.parse_args()
+    
+if __name__ == "__main__":
+    arguments = get_arguments()
+    
+    try:
+        parsed_file_a1 = re.findall('windows.(\d+)bpX(\d+)bp.([a-zA-Z]+)_([a-zA-Z]+).csv\Z', arguments.file_a[0])[0]
+        parsed_file_a2 = re.findall('windows.(\d+)bpX(\d+)bp.([a-zA-Z]+)_([a-zA-Z]+).csv\Z', arguments.file_a[1])[0]
+        parsed_file_b1 = re.findall('windows.(\d+)bpX(\d+)bp.([a-zA-Z]+)_([a-zA-Z]+).csv\Z', arguments.file_b[0])[0]
+        parsed_file_b2 = re.findall('windows.(\d+)bpX(\d+)bp.([a-zA-Z]+)_([a-zA-Z]+).csv\Z', arguments.file_b[1])[0]
+    except IndexError as err:
+        raise MetageneError(err, "You must specify two files for each group -a and -b")
+        
+    window_size = int(parsed_file_a1[0])
+    step_size = int(parsed_file_a1[1])
+     
+    subprocess.call(['Rscript', '-e', 'multiple_t_test.R', '--args', 
+                     arguments.file_a[0],        # file.1.sense
+                     arguments.file_a[1],        # file.1.antisense
+                     arguments.file_b[0],        # file.2.sense
+                     arguments.file_b[1],        # file.2.antisense
+                     arguments.normalization_a,  # normalization.1
+                     arguments.normalization_b,  # normalization.2
+                     arguments.output,           # output.prefix
+                     window_size,                # window.size
+                     step_size,                  # window.step
+                     arguments.feature_counted]) # feature.name
+                     
+    print "Finished plotting"
---
+++ multiple_t_test.R
@@ -0,0 +1,186 @@
+# multiple_t_test.R contains the function (multi.t.test) which will perform
+# pairwise Welch's t-tests for each window, apply a Holm-Bonferoni correction
+# (alpha = 0.05) and plot the sum of the metagene data along with marks of the
+# significantly different windows.
+# Please see README for full details and examples.
+#
+# Requires:
+#     R (http://cran.us.r-project.org/)
+# 
+# Joy-El R.B. Talbot Copyright (c) 2014
+# 
+# The MIT License (MIT)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+args <- commandArgs(trailingOnly = TRUE)
+
+multi.t.test <- function (file.1.sense,
+                          file.1.antisense,
+                          file.2.sense, 
+                          file.2.antisense,
+                          normalization.1, 
+                          normalization.2, 
+                          output.prefix, 
+                          window.size, 
+                          window.step,
+                          feature.name) {
+    options(scipen=9999) # keep things out of scientific notation
+    million = 1000000
+    
+    # load data
+    windows.1.sense <- read.table(file.1.sense, header=TRUE, sep=",")
+    windows.1.sense$Window <- factor(windows.1.sense$Window)
+    total.windows <- length(levels(windows.1.sense$Window))
+    
+    windows.1.antisense <- read.table(file.1.antisense, header=TRUE, sep=",")
+    windows.1.antisense$Window <- factor(windows.1.antisense$Window)
+    
+    windows.2.sense <- read.table(file.2.sense, header=TRUE, sep=",")
+    windows.2.sense$Window <- factor(windows.2.sense$Window)
+    
+    windows.2.antisense <- read.table(file.2.antisense, header=TRUE, sep=",")
+    windows.2.antisense$Window <- factor(windows.2.antisense$Window)
+
+    # create summary file for plots
+    windows.sum <- aggregate(windows.1.sense$Abundance, by=list(windows.1.sense$Window), FUN=sum)
+    names(windows.sum) <- c("Window", "windows.1.sense")
+    
+    windows.sum$windows.2.sense <- aggregate(windows.2.sense$Abundance, by=list(windows.2.sense$Window), FUN=sum)$x
+    # make the antisense negative to invert the axes...
+    windows.sum$windows.1.antisense <- -(aggregate(windows.1.antisense$Abundance, by=list(windows.1.antisense$Window), FUN=sum)$x)
+    windows.sum$windows.2.antisense <- -(aggregate(windows.2.antisense$Abundance, by=list(windows.2.antisense$Window), FUN=sum)$x)
+    
+    # remove factoring from windows
+    windows.sum$Window <- 0:(total.windows - 1) # keep in parentheses for order of operations!!
+    
+    # Normalize windows
+    windows.sum[,c("windows.1.sense","windows.1.antisense")] <- (windows.sum[,c("windows.1.sense","windows.1.antisense")] / normalization.1) * million   
+    windows.sum[,c("windows.2.sense","windows.2.antisense")] <- (windows.sum[,c("windows.2.sense","windows.2.antisense")] / normalization.2) * million
+    
+    # Define min and max positions for the plot - scale up to nearest 250
+    max.yaxis <- ceiling(max(windows.sum[,c("windows.1.sense","windows.2.sense")]) / 250) * 250
+    min.yaxis <- floor(min(windows.sum[,c("windows.1.antisense","windows.2.antisense")]) / 250) * 250
+    
+    # Normalize the raw data in preparation for the t-tests
+    windows.1.sense$Abundance <- (windows.1.sense$Abundance / normalization.1) * million
+    windows.1.antisense$Abundance <- (windows.1.antisense$Abundance / normalization.1) * million
+    windows.2.sense$Abundance <- (windows.2.sense$Abundance / normalization.2) * million
+    windows.2.antisense$Abundance <- (windows.2.antisense$Abundance / normalization.2) * million
+    
+    # repeat for sense vs antisense
+    # t-test calculation for each window position
+    # Null Hypothesis = same mean (mu=0)
+    # unpaired, unequal variance, two-sided t-test (Welch Two Sample t-test)
+    t.test.results.sense <- data.frame(Window = 0:(total.windows-1),
+        p.value = sapply(0:(total.windows-1), function (x,m,n) t.test(m[m$Window == x, "Abundance"], n[n$Window == x, "Abundance"], paired=FALSE, var.equal = FALSE, conf.level = 0.95, mu = 0, alternative = "two.sided")$p.value, m=windows.1.sense, n=windows.2.sense))
+
+    # Holm-Bonferoni correction for multiple hypotheses
+    #(wikipedia.org/wiki/Holm-Bonferroni_method)
+    #    1. Order the p-values (and their respective null hypotheses) from smallest to largest H(1)...H(m); P(1)...P(m)
+    #    2. Find the minimal index k where P(k) > alpha / (m + 1 - K)
+    #    3. Reject the null hypotheses H(1)...H(k-1); fail to reject the hypotheses H(k)...H(m)
+
+    # sort by p.value
+    t.test.correction.sense <- t.test.results.sense[order(t.test.results.sense$p.value),]
+    t.test.correction.sense$k <- 1:total.windows
+
+    m <- total.windows
+    alpha <- 0.05
+
+    t.test.correction.sense$cutoff <- alpha / (m + 1 - t.test.correction.sense$k)
+
+    t.test.correction.sense$reject.null <- t.test.correction.sense$p.value < t.test.correction.sense$cutoff
+
+
+    t.test.results.antisense <- data.frame(Window = 0:(total.windows-1),
+        p.value = sapply(0:(total.windows-1), function (x,m,n) t.test(m[m$Window == x, "Abundance"], n[n$Window == x, "Abundance"], paired=FALSE, var.equal = FALSE, conf.level = 0.95, mu = 0, alternative = "two.sided")$p.value, m=windows.1.antisense, n=windows.2.antisense))
+
+    # Holm-Bonferoni correction for multiple hypotheses
+    #(wikipedia.org/wiki/Holm-Bonferroni_method)
+    #    1. Order the p-values (and their respective null hypotheses) from smallest to largest H(1)...H(m); P(1)...P(m)
+    #    2. Find the minimal index k where P(k) > alpha / (m + 1 - K)
+    #    3. Reject the null hypotheses H(1)...H(k-1); fail to reject the hypotheses H(k)...H(m)
+
+    # sort by p.value
+    t.test.correction.antisense <- t.test.results.antisense[order(t.test.results.antisense$p.value),]
+    t.test.correction.antisense$k <- 1:total.windows
+
+    m <- total.windows
+    alpha <- 0.05
+
+    t.test.correction.antisense$cutoff <- alpha / (m + 1 - t.test.correction.antisense$k)
+
+    t.test.correction.antisense$reject.null <- t.test.correction.antisense$p.value < t.test.correction.antisense$cutoff
+
+
+    # outputs
+    write.table(t.test.correction.sense, 
+                file = paste(output.prefix,".sense_pvalues.csv",sep=""),
+                quote = FALSE,
+                col.names = TRUE,
+                row.names = FALSE,
+                sep = "\t")
+    write.table(t.test.correction.antisense, 
+                file = paste(output.prefix,".antisense_pvalues.csv",sep=""),
+                quote = FALSE,
+                col.names = TRUE,
+                row.names = FALSE,
+                sep = "\t")            
+    
+    has.feature <- data.frame(Window = 0:(total.windows - 1),
+                              Start = 0:(total.windows -1) * window.step,
+                              End   = (0:(total.windows -1) * window.step) + window.size - 1)
+    feature.range <- (range(has.feature[has.feature$Start <= 1000 & 1000 <= has.feature$End, "Window"])) * window.step - 1000
+                     
+    pdf(paste(output.prefix, ".plot.pdf", sep=""))
+    plot((windows.sum$Window * window.step) - 1000, windows.sum$windows.1.sense, type="n", ylim=c(min.yaxis, max.yaxis), xlab=paste("Start position of ",window.size,"bp window relative to ",feature.name," (nt)", sep=""), ylab="Window Coverage in Reads per Million Mapped")
+    abline(v=feature.range[1], col="darkgrey")
+    abline(v=feature.range[2], col="darkgrey")
+    abline(h=0, col="darkgrey")
+    points((windows.sum$Window * window.step) - 1000, windows.sum$windows.1.sense, type="l", col="blue", lwd=2)
+    points((windows.sum$Window * window.step) - 1000, windows.sum$windows.1.antisense, type="l", col="blue", lwd=2)
+    points((windows.sum$Window * window.step) - 1000, windows.sum$windows.2.sense, type="l", col="green", lwd=2)
+    points((windows.sum$Window * window.step) - 1000, windows.sum$windows.2.antisense, type="l", col="green", lwd=2)
+
+    # add segments for statistically signficant regions
+
+    segments(x0=(c(t.test.correction.sense[t.test.correction.sense$reject.null,"Window"]) * window.step) - 1000, 
+             y0=rep(0.99 * max.yaxis, sum(t.test.correction.sense$reject.null)), 
+             x1=(c(t.test.correction.sense[t.test.correction.sense$reject.null,"Window"]) * window.step) - 1000 + window.size, 
+             col="red", lwd=3)
+    
+    segments(x0=(c(t.test.correction.antisense[t.test.correction.antisense$reject.null,"Window"]) * window.step) - 1000, 
+             y0=rep(0.99 * min.yaxis, sum(t.test.correction.antisense$reject.null)), 
+             x1=(c(t.test.correction.antisense[t.test.correction.antisense$reject.null,"Window"]) * window.step) - 1000 + window.size, 
+             col="red", lwd=3)
+    dev.off()
+}
+
+
+multi.t.test(args[1], #file.1.sense
+             args[2], #file.1.antisense
+             args[3], #file.2.sense 
+             args[4], #file.2.antisense
+             args[5], #normalization.1, 
+             args[6], #normalization.2, 
+             args[7], #output.prefix, 
+             args[8], #window.size, 
+             args[9], #window.step,
+             args[10]) #feature.name)
